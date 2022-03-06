c99593bbd4b49bfa2516e91c55722740a6a7e690
xiaogaogao75 <96165921+xiaogaogao75@users.noreply.github.com>
2022-03-06 15:40:05 +0800

    数据类型，数据变量

---
+++ tect_1 .cpp
@@ -0,0 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+/*
+ctrl+f5 快速调试
+数据类型
+ 
+char	字符数据类型
+short	短整形	
+int		整形
+long	长整形	
+long long更长的整形
+float单精度浮点数
+double双精度浮点数
+
+sizeof--关键字--操作符--计算类型或者变量所占空间的大小 单位字节
+	printf("%zd\n", sizeof(char));	1
+  	printf("%zd\n", sizeof(short));	2
+	printf("%zd\n", sizeof(int));	4
+	printf("%zd\n", sizeof(long));	4
+	printf("%zd\n", sizeof(long long));	8
+	printf("%zd\n", sizeof(float));	4
+	printf("%zd\n", sizeof(float));	4	
+	printf("%zd\n", sizeof(double));	8
+
+*/
+//int main()
+//{									//字符数据类型
+//	char ch = 'a';	//初始化
+//	//整形
+//	int age = 20;
+//	//短整形
+//	short num = 10;
+//	//单精度浮点数
+//	//float weight = 55.55;
+//	//双精度浮点数
+//	double	weight = 90;
+//
+//	weight = weight - 20;
+//	age = age + 1;
+//	printf("%d\n", age);
+//	printf("%f\n", weight);
+//
+//	return 0;
+//}
+
+
+//int a = 100;
+//int main()
+//{		/*
+//			全局变量：在main前面
+//			局部变量：函数体内
+//			局部变量与全局变量有冲突的时候优先局部变量 
+//		*/
+//	int a = 20;
+//	printf("%d", a);
+//	return 0;
+//}
+
+//int main()
+//{	
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	scanf("%d %d", &a, &b);
+//	sum = a + b;
+//	printf("sum=%d\n",sum);
+//	return 0;
+//}
+
+int main()
+{
+	return 0;
+}
