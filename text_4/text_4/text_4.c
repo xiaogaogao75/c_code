8152c74aeef86c003b9ca94ad6cb68c347a99187
xiaogaogao <2839033900@qq.com>
2022-03-13 08:36:18 +0800

    初识转义字符和注释

---
+++ text_4/text_4/text_4.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+		//ASCII表在text_4文件夹中
+//int main()
+//{
+//	printf("C:\text.c，\n"); //\t是转义字符代表tab键   \0,\n都是
+//
+//	printf("%c\n",'\'');
+//	printf("%s\n", "\"");
+//
+//	return 0;
+//}
+//
+		//转义字符表在text_4文件夹中
+//int main()
+//{
+//	printf("%c,\n", '\130');
+//	printf("%c,\n", '\101');
+//	printf("%c,\n",'\x30');
+//	return 0;
+//}
