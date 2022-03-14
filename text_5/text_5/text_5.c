c64abe4656ab96e635dbe6d37d8ccfc963b423b6
xiaogaogao <2839033900@qq.com>
2022-03-14 08:23:27 +0800

    初识操作符，初识关键字

--- text_5/text_5/text_5.c
+++ text_5/text_5/text_5.c
@@ -161,17 +161,17 @@
 //exp1成立 ，exp2计算，整个表达式的结果是exp2的结果
 //exp1不成立 ，exp3计算，整个表达式的结果是exp3的结果
 
-int main()
-{
-	int a = 0;
-	int b = 3;
-	int max = 0;
-	/*if (a > b)
-		max = a; 
-	else
-		max = b;*/
-
-	max = a > b ? a : b;
-	printf("%d\n", max);
-	return 0;
-}
+//int main()
+//{
+//	int a = 0;
+//	int b = 3;
+//	int max = 0;
+//	/*if (a > b)
+//		max = a; 
+//	else
+//		max = b;*/
+//
+//	max = a > b ? a : b;
+//	printf("%d\n", max);
+//	return 0;
+//}
