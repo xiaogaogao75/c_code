e12b4bbb212336f1e711e415b8dedd3e4f4cad5d
xiaogaogao75 <96165921+xiaogaogao75@users.noreply.github.com>
2022-03-24 22:33:00 +0800

    Add files via upload

---
+++ text_9.c
@@ -0,0 +1,43 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	char passward[20] = { 0 };
+//	printf("请输入密码:");
+//	scanf("%s", passward);
+//	printf("请确认密码(Y/N):>");
+//	//getchar();  清理单个字符的缓冲区这里清理/n
+//	int tem = 0;
+//	//清理缓冲区多个字符 while
+//	while ((tem = getchar()) !='\n')
+//	{
+//		;
+//	}
+//	int ch = getchar();
+//	if (ch=='Y')
+//	{
+//		printf("确认成功\n");
+//	}
+//	else
+//	{
+//		printf("确认失败\n");
+//	}
+//	return 0;
+//}
+//
+
+
+//int	main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if (ch > '0' || ch < '9')
+//			continue;
+//		putchar(ch);
+//
+//	}
+//	
+//	return 0;
+//}
