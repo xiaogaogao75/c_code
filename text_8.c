44b51de119694c816890f397758ce10c40107718
xiaogaogao75 <96165921+xiaogaogao75@users.noreply.github.com>
2022-04-07 22:03:46 +0800

    while循环

---
+++ text_8.c
@@ -0,0 +1,36 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 1;
+//
+//	//在while循环中break用于永久终止循环
+//	//在while循环中continue用于跳过本次循环continue后面的代码，直接判断
+//	/*while (a<=10)
+//	{
+//		if (a == 5)
+//			break;
+//		printf("%d ", a);
+//		a++;
+//	}*/
+//	while (a <= 10)
+//	{
+//		if (a == 5)
+//			continue;
+//		printf("%d ", a);
+//		a++;
+//	}
+//	return 0;
+//}
+
+
+//getchar是读取字符
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar())	!= EOF)
+//	{
+//		putchar(ch);
+//	}
+//	return 0;
+//}
